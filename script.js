2058f5651ba6baaad6c3ad01844b9778878540e9
John Telban <Jtelban1@gmail.com>
2020-02-11 20:23:53 -0700

    first push

---
+++ script.js
@@ -0,0 +1,86 @@
+
+
+const questionArr = [
+
+    {
+        question: "Commonly used data types DO NOT include:",
+        answers: ["strings, "Booleans", "alerts---correct", numbers"]
+        correctAnswer: 3
+    },
+    {
+        question: "The condition in an if/else statement is enclosed within___.?",
+        answers: ["var, "const and let", "variable=", idk"]
+        correctAnswer: 1
+    },
+    {
+        question: "How dp you define a variable in ES6?",
+        answers: ["var, "const and let", "variable=", idk"]
+        correctAnswer: 1
+    },
+    {
+        question: "How dp you define a variable in ES6?",
+        answers: ["var, "const and let", "variable=", idk"]
+        correctAnswer: 1
+    },
+    {
+        question: "How dp you define a variable in ES6?",
+        answers: ["var, "const and let", "variable=", idk"]
+        correctAnswer: 1
+    },
+
+    
+];
+
+
+
+// starting the quiz
+  // A. Create button (onClick function )
+      // i. hide the start div
+      // ii. create the first question(createQFunc)
+        // - grab element by id
+        // - create question
+        // Do the same ofrall five questions
+
+// b. The quiz
+
+
+const startQuizDiv = document.querySelector("#startQuiz")
+var startButton = document.querySelector("#startButton")
+
+// Start Button onclick
+ 
+startButton.addEventListener(click, function(){
+    // A. Create button (onClick function )
+      // i. hide the start div
+      // ii. create the first question(createQFunc)
+        // - grab element by id
+        // - create question
+        // Do the same ofrall five questions
+
+
+})
+
+//create Question Func
+Function createQuestion() {
+
+    // grab quiz div
+    // create question <h1>
+    //each answer needs to be a button
+    //define the click event
+    // loop to create the answer btns
+
+}
+// choose answer event listner
+
+// main quiz function
+function startQuiz(){
+
+    let currentQs = questionArr;
+    
+    //some sort of loop
+
+    //create the question
+    // call createQuestion
+
+}
+
