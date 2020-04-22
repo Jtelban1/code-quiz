2a64da7d15074ba5399c9a7c83300f0f7a6d6e1a
John Telban <Jtelban1@gmail.com>
2020-04-21 20:37:26 -0700

    Index and JS

--- script.js
+++ script.js
@@ -1,86 +1,121 @@
-
-
 const questionArr = [
-
     {
-        question: "Commonly used data types DO NOT include:",
-        answers: ["strings, "Booleans", "alerts---correct", numbers"]
-        correctAnswer: 3
+        question: "Inside which HTML element do we put the JavaScript?:",
+        answers: ["<js>", "<Script>", "<Scripting>", "none of these"],
+        correctAnswer: 2,
     },
     {
-        question: "The condition in an if/else statement is enclosed within___.?",
-        answers: ["var, "const and let", "variable=", idk"]
-        correctAnswer: 1
+        question: "Where is the correct place to insert a JavaScript?",
+        answers: [
+            "The <head> section",
+            "The <body> section",
+            "Both",
+            "Neither",
+        ],
+        correctAnswer: 1,
     },
     {
-        question: "How dp you define a variable in ES6?",
-        answers: ["var, "const and let", "variable=", idk"]
-        correctAnswer: 1
+        question: "How do you define a variable in ES6?",
+        answers: ["var", "const and let", "variable=", "none of these"],
+        correctAnswer: 2,
     },
     {
-        question: "How dp you define a variable in ES6?",
-        answers: ["var, "const and let", "variable=", idk"]
-        correctAnswer: 1
+        question: "Choose the correct HTML element for the largest heading:?",
+        answers: ["<h1>", "<h6>", "<heading>", "<head>"],
+        correctAnswer: 1,
     },
-    {
-        question: "How dp you define a variable in ES6?",
-        answers: ["var, "const and let", "variable=", idk"]
-        correctAnswer: 1
-    },
-
-    
 ];
 
+let givenAnswers;
 
-
-// starting the quiz
-  // A. Create button (onClick function )
-      // i. hide the start div
-      // ii. create the first question(createQFunc)
-        // - grab element by id
-        // - create question
-        // Do the same ofrall five questions
-
-// b. The quiz
-
-
-const startQuizDiv = document.querySelector("#startQuiz")
-var startButton = document.querySelector("#startButton")
+const startQuizDiv = document.querySelector("#startQuiz");
+var startButton = document.querySelector("#startButton");
 
 // Start Button onclick
- 
-startButton.addEventListener(click, function(){
-    // A. Create button (onClick function )
-      // i. hide the start div
-      // ii. create the first question(createQFunc)
-        // - grab element by id
-        // - create question
-        // Do the same ofrall five questions
 
-
-})
+startButton.addEventListener("click", function () {
+    givenAnswers = [];
+    resultDiv.innerHTML = "";
+    startQuizDiv.style.display = "none";
+    quizDiv.style.display = "flex";
+    createQuestion(0);
+});
 
 //create Question Func
-Function createQuestion() {
+const quizDiv = document.querySelector("#quizDiv");
+const answers = [];
 
-    // grab quiz div
-    // create question <h1>
-    //each answer needs to be a button
-    //define the click event
-    // loop to create the answer btns
+function createQuestion(index) {
+    let questionHTML = document.createElement("div");
+    questionHTML.classList.add("card");
+    questionHTML.classList.add("column");
+    let box = document.createElement("div");
+    box.classList.add("card-content");
+    questionHTML.appendChild(box);
+    let h2 = document.createElement("h2");
+    h2.classList.add("subtitle");
+    h2.innerText = questionArr[index].question;
+    box.appendChild(h2);
+    questionArr[index].answers.forEach((answer, i) => {
+        answers[i] = document.createElement("button");
+        answers[i].className = "button";
+        answers[i].innerText = answer;
+        answers[i].dataset.answer = i;
+        answers[i].dataset.question = index;
+        answers[i].addEventListener("click", evaluateAnswer);
+        box.appendChild(answers[i]);
+    });
+    // remove old
+    let old = document.querySelector(".card");
+    if (old) {
+        quizDiv.removeChild(old);
+    }
 
+    // attach new
+    quizDiv.appendChild(questionHTML);
 }
-// choose answer event listner
 
-// main quiz function
-function startQuiz(){
+const resultDiv = document.querySelector("#results");
+if (typeof localStorage.lastResults === "undefined") {
+    localStorage.setItem("lastResults", "[]");
+}
+function displayResults() {
+    resultDiv.innerHTML = '<div class="result-box"></div>';
+    /*resultDiv.childNodes.forEach(child =>{
+        resultDiv.removeChild(child);
+    })*/
 
-    let currentQs = questionArr;
-    
-    //some sort of loop
-
-    //create the question
-    // call createQuestion
-
+    let formerResultHeadline = document.createElement("h3");
+    formerResultHeadline.classList.add("subtitle");
+    formerResultHeadline.innerText = "Your previous results:";
+    resultDiv.firstChild.appendChild(formerResultHeadline);
+    let formerResults = JSON.parse(localStorage.lastResults);
+    for (let i = formerResults.length - 1; i >= 0; i--) {
+        let div = document.createElement("div");
+        div.innerText = "Attempt " + (i + 1) + ": " + formerResults[i] + " %";
+        resultDiv.firstChild.appendChild(div);
+    }
 }
 
+function evaluateAnswer(ev) {
+    // save to evaluation
+    let correct =
+        Number(questionArr[ev.target.dataset.question].correctAnswer) ===
+        Number(ev.target.dataset.answer);
+    givenAnswers.push(correct);
+    // does next question exist?
+    let nextIndex = Number(ev.target.dataset.question) + 1;
+    if (typeof questionArr[nextIndex] !== "undefined") {
+        createQuestion(nextIndex);
+    } else {
+        let percent =
+            (givenAnswers.filter((val) => val).length / questionArr.length) *
+            100;
+        let formerResults = JSON.parse(localStorage.lastResults);
+        formerResults.push(percent);
+        localStorage.setItem("lastResults", JSON.stringify(formerResults));
+        startQuizDiv.style.display = "flex";
+        quizDiv.style.display = "none";
+        displayResults();
+    }
+}
